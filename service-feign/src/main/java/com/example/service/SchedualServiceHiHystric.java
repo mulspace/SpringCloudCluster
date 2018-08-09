367283048eba443c4be396a23a8c16e5d1553005
Kevin Fu <jifu@cisco.com>
2018-08-09 14:05:46 +0800

    init version of the demo

---
+++ service-feign/src/main/java/com/example/service/SchedualServiceHiHystric.java
@@ -0,0 +1,14 @@
+
+package com.example.service;
+
+import org.springframework.stereotype.Component;
+
+
+
+@Component
+public class SchedualServiceHiHystric implements SchedualServiceHi {
+    @Override
+    public String sayHiFromClientOne(String name) {
+        return "sorry "+name;
+    }
+}
